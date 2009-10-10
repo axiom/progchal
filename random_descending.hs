c0f7cc7ab3db23eac5e517a76d253c1bacdaef50
Johannes Martinsson <johannes@antiklimax.se>
2009-10-11 01:14:35 +0200

    broken submission system?

--- random_descending.hs
+++ random_descending.hs
@@ -1,10 +1,15 @@
-import Text.Printf
+{-# LANGUAGE BangPatterns #-}
+import Text.Printf (printf)
 
-expected :: String -> Integer -> Double -> Double
-expected [] _ acc = acc
-expected (c:s) h acc = case c of
-	'(' -> expected s (h + 1) acc
-	')' -> expected s (h - 1) acc
-	_   -> let (v, r) = (head . reads) (c:s) in expected r h $ acc + v * 0.5^h
+expected :: Integer -> Double -> String -> Double
+expected _  !acc []    = acc
+expected !h !acc (c:s) = case c of
+	'\n' -> expected h acc s
+	' '  -> expected h acc s
+	'('  -> expected (h + 1) acc s
+	')'  -> expected (h - 1) acc s
+	_    -> let (v, r) = (head . reads) (c:s) in
+		expected h (acc + v * 0.5^h) r
 
-main = getLine >>= \l -> let v = expected l 0 0 in printf "%.2f\n" v
+main :: IO ()
+main = getContents >>= printf "%.2f\n" . expected 0 0
