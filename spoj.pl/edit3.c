5ba95854679d9c8459a513b4e0257656d82c355b
Johannes Martinsson <johannes@antiklimax.se>
2009-11-18 21:32:37 +0100

    linked list implementation of edit3, still too slow, input hang?

--- spoj.pl/edit3.c
+++ spoj.pl/edit3.c
@@ -2,12 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX 2048000
+struct c_element {
+	char val;
+	struct c_element *next;
+	struct c_element *prev;
+};
+
+typedef struct c_element item;
+
+void insert(int n);
+void delete(int n);
+void get(int n);
+void move(int n);
+void next();
+void prev();
+
+void init();
+void clear();
+
+static item *root;
+static item *point;
 
 int main()
 {
-	unsigned int cases, ops, arg, cur, mc, i;
-	char text[MAX];
+	unsigned int cases, ops, arg;
 
 	scanf("%u", &cases);
 
@@ -15,60 +33,167 @@ int main()
 		scanf("%u", &ops);
 		getchar(); // Munch up newline character.
 
-		cur = 0;
-		mc  = 0;
+		init();
 
 		while (ops--) {
 			switch (getchar()) {
 
 				case 'I':
 					scanf("nsert %u", &arg);
-
-					memmove((text + cur + arg), (text + cur), (mc - cur));
 					getchar();
-
-					for (i = cur; i < cur + arg; i++) {
-						text[i] = getchar();
-						if (text[i] == '\n') i--;
-					}
-
-					mc += arg;
+					insert(arg);
 					getchar();
 					continue;
 
 				case 'D':
 					scanf("elete %u", &arg);
-					mc -= arg;
-					memmove((text + cur), (text + cur + arg), (mc - cur));
+					delete(arg);
 					getchar();
 					continue;
 
 				case 'G':
 					scanf("et %u", &arg);
-					printf("%.*s\n", arg, (text + cur));
+					get(arg);
 					getchar();
 					continue;
 
 				case 'M':
 					scanf("ove %u", &arg);
-					cur = arg;
+					move(arg);
 					getchar();
 					continue;
 
 				case 'N':
 					fseek(stdin, 4, SEEK_CUR);
-					cur++;
+					next();
 					continue;
 
 				case 'P':
 					fseek(stdin, 4, SEEK_CUR);
-					cur--;
+					prev();
 					continue;
 			}
 		}
+
+		clear();
 
 		getchar(); // Munch up newline character.
 	}
 
 	return 0;
+}
+
+void next()
+{
+	if (point->next != NULL)
+		point = point->next;
+}
+
+void prev()
+{
+	if (point->prev != NULL)
+		point = point->prev;
+}
+
+void move(int n)
+{
+	point = root;
+	while (n--) next();
+}
+
+void get(int n)
+{
+	item *prt = point;
+	item *old = point;
+
+	while (n-- && prt->next != NULL) {
+		prt = prt->next;
+		putc(prt->val, stdout);
+	}
+
+	putc('\n', stdout);
+	point = old;
+}
+
+void delete(int n)
+{
+	item *del;
+
+	while (n--) {
+		del = point->next;
+
+		if (del != NULL && del->next != NULL) {
+			del->next->prev = del->prev;
+			del->prev->next = del->next;
+			free(del);
+		}
+	}
+}
+
+void insert(int n)
+{
+	char c;
+	item *cur, *old;
+
+	cur = NULL;
+	old = NULL;
+
+	while (n--) {
+		c = getchar();
+
+		// Pretend we don't see newline characters.
+		if (c == '\n') {
+			n++;
+			continue;
+		}
+
+		cur = (item *) malloc(sizeof(item));
+		cur->val = c;
+
+		// This is not the first character inserted, link this to previous one
+		// and vice versa.
+		if (old != NULL) {
+			cur->prev = old;
+			cur->next = old->next;
+
+			if (old->next != NULL)
+				old->next->prev = cur;
+
+			old->next = cur;
+		}
+
+		// First element inserted this time, link cursor's next to this first
+		// character, and it to the cursor.
+		else {
+			cur->prev = point;
+			cur->next = point->next;
+
+			if (point->next != NULL)
+				point->next->prev = cur;
+
+			point->next = cur;
+		}
+
+		old = cur;
+	}
+}
+
+void clear()
+{
+	item *temp = point;
+
+	while (point) {
+		temp = point;
+		point = point->next;
+		free(temp);
+	}
+}
+
+void init()
+{
+	root = (item *) malloc(sizeof(item));
+	root->val = '\0';
+	root->prev = NULL;
+	root->next = NULL;
+	point = root;
 }
